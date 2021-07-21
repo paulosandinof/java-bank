24b5e6a9c525caf983f1f83348b00b60f9215626
Paulo Sandino <paulosandino@gmail.com>
2021-07-20 21:17:15 -0300

    created the main classes of the project, with methods to create accounts

---
+++ src/main/java/com/sandino/Account.java
@@ -0,0 +1,28 @@
+package com.sandino;
+
+public class Account {
+
+    private int number;
+    private double balance;
+
+    public Account(int number) {
+        this.number = number;
+        this.balance = 0;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
--- src/main/java/com/sandino/App.java
+++ src/main/java/com/sandino/App.java
@@ -1,7 +1,29 @@
 package com.sandino;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        int option = 0;
+        Scanner input = new Scanner(System.in);
 
+        Bank bank = new Bank();
+
+        while(option != 6) {
+            System.out.println("1 - Create account: ");
+            System.out.println("6 - Exit: ");
+            System.out.print("Choose an option: ");
+            
+            option = input.nextInt();
+
+            if (option == 1) {
+                System.out.print("Please type a number: ");
+                
+                int number = input.nextInt();
+                
+                bank.createAccount(number);
+            }
+        }
+        input.close();
     }
 }
