41734d4ab60a68a0fb4f65a26170a30963bdcfba
Paulo Sandino <paulosandino@gmail.com>
2021-07-28 11:13:23 -0300

    Created the methods to handle saving accounts creation and interest earning

--- src/main/java/com/sandino/Bank.java
+++ src/main/java/com/sandino/Bank.java
@@ -34,6 +34,19 @@ public class Bank {
         System.out.println("Account created");
     }
 
+    public void createSavingsAccount(int number) {
+        boolean exists = accounts.stream().anyMatch(account -> account.getNumber() == number);
+
+        if (exists) {
+            System.out.println("An account with this number already exists");
+            return;
+        }
+
+        SavingsAccount savingsAccount = new SavingsAccount(number);
+        accounts.add(savingsAccount);
+        System.out.println("Account created");
+    }
+
     public void checkBalance(int number) {
         Account existingAccount = accounts.stream().filter(account -> account.getNumber() == number).findFirst()
                 .orElse(null);
@@ -97,5 +110,26 @@ public class Bank {
 
         System.out.println("The amount of " + amount + " was transfered from account " + existingSender.getNumber()
                 + " to account " + existingReceiver.getNumber());
+    }
+
+    public void earnInterest(int number, double percentage) {
+        Account existingAccount = accounts.stream().filter(account -> account.getNumber() == number).findFirst()
+                .orElse(null);
+
+        if (existingAccount == null) {
+            System.out.println(ERROR_MESSAGE);
+            return;
+        }
+
+        if (!(existingAccount instanceof SavingsAccount)) {
+            System.out.println("This operation cannot be done by this account");
+            return;
+        }
+
+        SavingsAccount savingsAccount = (SavingsAccount) existingAccount;
+
+        savingsAccount.earnInterest(percentage);
+
+        System.out.println("The account " + savingsAccount.getNumber() + " earned " + percentage + "% of interest");
     }
 }
