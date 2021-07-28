32f522daf500a91a397ccffb360aa6af145ba8a9
Paulo Sandino <paulosandino@gmail.com>
2021-07-28 11:12:19 -0300

    Added the options to create a savings account and earn interest on the menu

--- src/main/java/com/sandino/App.java
+++ src/main/java/com/sandino/App.java
@@ -4,19 +4,22 @@ import java.util.Scanner;
 
 public class App {
     private static final String SEPARATOR = "-----------------------------";
+
     public static void main(String[] args) {
         int option = 0;
         Scanner input = new Scanner(System.in);
 
         Bank bank = new Bank();
 
-        while (option != 6) {
+        while (option != 8) {
             System.out.println("1 - Create account");
-            System.out.println("2 - Check Balance");
-            System.out.println("3 - Deposit");
-            System.out.println("4 - Withdraw");
-            System.out.println("5 - Transfer");
-            System.out.println("6 - Exit");
+            System.out.println("2 - Create savings account");
+            System.out.println("3 - Check Balance");
+            System.out.println("4 - Deposit");
+            System.out.println("5 - Withdraw");
+            System.out.println("6 - Transfer");
+            System.out.println("7 - Earn interest");
+            System.out.println("8 - Exit");
             System.out.print("Choose an option: ");
 
             option = input.nextInt();
@@ -33,24 +36,19 @@ public class App {
                 System.out.println(SEPARATOR);
 
             } else if (option == 2) {
-                System.out.print("Please type an account number: ");
+                System.out.print("Please type a number: ");
 
                 int number = input.nextInt();
 
-                bank.checkBalance(number);
+                bank.createSavingsAccount(number);
 
                 System.out.println(SEPARATOR);
-
             } else if (option == 3) {
                 System.out.print("Please type an account number: ");
 
                 int number = input.nextInt();
 
-                System.out.print("Please type the amount: ");
-                
-                double amount = input.nextDouble();
-
-                bank.deposit(number, amount);
+                bank.checkBalance(number);
 
                 System.out.println(SEPARATOR);
 
@@ -60,14 +58,27 @@ public class App {
                 int number = input.nextInt();
 
                 System.out.print("Please type the amount: ");
-                
+
                 double amount = input.nextDouble();
 
-                bank.withdraw(number, amount);
-            
+                bank.deposit(number, amount);
+
                 System.out.println(SEPARATOR);
 
             } else if (option == 5) {
+                System.out.print("Please type an account number: ");
+
+                int number = input.nextInt();
+
+                System.out.print("Please type the amount: ");
+
+                double amount = input.nextDouble();
+
+                bank.withdraw(number, amount);
+
+                System.out.println(SEPARATOR);
+
+            } else if (option == 6) {
                 System.out.print("Please type the sender account number: ");
                 int sender = input.nextInt();
 
@@ -78,6 +89,19 @@ public class App {
                 double amount = input.nextDouble();
 
                 bank.transfer(sender, receiver, amount);
+
+                System.out.println(SEPARATOR);
+
+            } else if (option == 7) {
+                System.out.print("Please type an account number: ");
+
+                int number = input.nextInt();
+
+                System.out.print("Please type the percentage: ");
+
+                double percentage = input.nextDouble();
+
+                bank.earnInterest(number, percentage);
 
                 System.out.println(SEPARATOR);
 
